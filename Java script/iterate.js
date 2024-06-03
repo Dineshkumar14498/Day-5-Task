4abe5ab4d2398f70ed8872268fda0e6ad001f979
Dineshkumar14498 <dineshvlr9500@gmail.com>
2024-06-03 19:18:39 +0530

    completed

--- Java script/iterate.js
+++ Java script/iterate.js
@@ -1,30 +1,40 @@
-const resume={
-  "Name":"Dineshkumar",
-  "father Name":"Anbalagan",
-  "mother Name":"Lakshmi",
-  "Age":"26",
-  "Gender":"male",
-  "DOB":"14-04-1998",
-  "Nationality":"Indian",
+let resume = {
+  "Name": "Dineshkumar",
+  "father Name": "Anbalagan",
+  "mother Name": "Lakshmi",
+  "Age": "26",
+  "Gender": "male",
+  "DOB": "14-04-1998",
+  "Nationality": "Indian",
   "Qulification": "B.com",
-  "skills":["Javascript,HTML,Css"],
-  "Language Known": ["Tamil","English"],
-  "Email":"dineshvlr9500@gmail.com",
+  "skills": ["Javascript,HTML,Css"],
+  "Language Known": ["Tamil", "English"],
+  "Email": "dineshvlr9500@gmail.com",
   "phone": "9629144985",
-
 }
 
-console.log(resume.skills)
 
 
 //for in
 
 for (var self in resume) {
-  console.log(self + resume[self]);
+  console.log(`${self}: ${resume[self]}`);
 }
 
 //for of
 
-for (const value of resume) {
-  console.log(value);
+for (let [self,key] of Object.entries(resume)) {
+  console.log(`${self}: ${key}`);
 }
+
+//for each
+
+Object.entries(resume).forEach(([self,key]) => {
+
+  console.log(`${self}: ${key}`)
+  
+
+})
+
+
+
